310271f012c057f91a9b11934a24c97b0e62cec8
rbrandenberg-art <r.brandenberg@duocuc.cl>
2026-07-09 11:02:33 -0400

    Agrega un 7.0

--- src/componentes/CrudAdmin/PruebaCards.jsx
+++ src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,7 +11,7 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="Consultar Ordenes de compra 💰"
+          title="Consultar Ordenes de compra 💰 un 7.0"
           description="Revisa las últimas oc realizadas para generar su despacho"
           buttonText="Consultar"
           onClick={() => {
